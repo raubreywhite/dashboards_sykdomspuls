afa1f78888cf4d7e23720aa85fcb92ab0eb0da4f
Richard White <r@rwhite.no>
2018-10-12 09:10:52 +0300

    travis commit message

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,9 @@ addToDrat(){
 
   Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
     repodir = '.', \
-    commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
+    commit=FALSE)"
   Rscript -e "saveRDS(read.dcf('src/contrib/PACKAGES'),'src/contrib/PACKAGES.rds')"
-  git commit -a -m "Fixing PACKAGES.rds"
+  git commit -a -m "Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER"
   git push 2>err.txt
 
 }
