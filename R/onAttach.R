57df3a7a3de6993ca8d51a0f26512aaf9886235d
Richard White <w@rwhite.no>
2019-01-03 20:03:52 +0200

    Update onAttach.R

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,10 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: sykdomspuls")
-<<<<<<< HEAD
   packageStartupMessage("Version 2019.01.03 at 06:54")
-=======
-  packageStartupMessage("Version 2018.12.19 at 14:10")
->>>>>>> parent of 55caa35... Travis CI update 101 [ci skip]
   packageStartupMessage("Developed by Richard White")
   packageStartupMessage("Department of Infectious Disease Epidemiology and Modelling")
   packageStartupMessage("Norwegian Institute of Public Health")
