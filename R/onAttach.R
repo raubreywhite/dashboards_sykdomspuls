fa801099be125108f107f6d0e046d7ea4f2f63c1
Travis CI User <travis@example.org>
2019-01-07 11:55:45 +0000

    Travis CI update 127 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: sykdomspuls")
-  packageStartupMessage("Version 2019.01.04 at 08:30")
+  packageStartupMessage("Version 2019.01.07 at 11:34")
   packageStartupMessage("Developed by Richard White")
   packageStartupMessage("Department of Infectious Disease Epidemiology and Modelling")
   packageStartupMessage("Norwegian Institute of Public Health")
