590ccc90ec976da6f5bc61f2c42cba82321035a6
Travis CI User <travis@example.org>
2019-03-06 16:17:57 +0000

    Travis CI update 151 [ci skip]

--- R/AnalysesSecondary.R
+++ R/AnalysesSecondary.R
@@ -7,13 +7,13 @@ AnalyseLog <- function() {
 
   log <- rbindlist(log, fill = T)
 
-  if(!"versionAlgorithm" %in% names(log)) log[,versionAlgorithm:=1]
+  if (!"versionAlgorithm" %in% names(log)) log[, versionAlgorithm := 1]
   log[is.na(versionAlgorithm), versionAlgorithm := 1]
 
-  if(!"versionPackage" %in% names(log)) log[,versionPackage:="Unknown"]
+  if (!"versionPackage" %in% names(log)) log[, versionPackage := "Unknown"]
   log[is.na(versionPackage), versionPackage := "Unknown"]
 
-  if(!"numTags" %in% names(log)) log[,numTags:=8]
+  if (!"numTags" %in% names(log)) log[, numTags := 8]
   log[is.na(numTags), numTags := 8]
   log[, date := as.Date(format.Date(initialiseBefore, "%Y-%m-%d"))]
 
