8c7eeea1702c3e6f6aedc7e867dd4fc60029f559
root <root@b39c92417928>
2019-06-28 15:06:42 +0000

    Basic plots and dashboard for mem influensa

--- R/Mem.R
+++ R/Mem.R
@@ -1,8 +1,10 @@
-#' Model classes
+#' Mem implementation
 #'
 #' @import data.table
 #' @import lubridate
 #' @import mem
+#' @import ggplot2
+#' @import fhi
 
 run_all <- function(conf){
 
@@ -16,6 +18,7 @@ add_columns <- function(data){
   data[, medium:=as.numeric(NaN)]
   data[, high:=as.numeric(NaN)]
   data[, very_high:=as.numeric(NaN)]
+  data[, season:= ""]
   
 }
 
@@ -24,6 +27,10 @@ add_results_to_data <- function(data, mem_results){
   for(yea in unique(data[, year])){
     label0_20 = paste(yea - 2, yea - 1, sep="/")
     label40_52 = paste(yea - 1, yea, sep="/")
+    
+    data[year==yea & week<=20, season:= paste(yea - 1, yea, sep="/")]
+    data[year==yea & week>=40, season:= paste(yea, yea + 1 , sep="/")]
+    
     if(! is.null(mem_results[[label0_20]][1])){
       data[year==yea & week<=20, low:= mem_results[[label0_20]][1]]
     }
@@ -107,6 +114,71 @@ run_all_mem <- function(conf){
 }
 
 
+#' get_season
+#'
+#' get the influensa season
+#' 
+#' @export get_season
+get_season <- function(date){
+  
+  year = year(date)
+  week = week(date)
+
+  if(week < 40){
+    season = paste(year - 1, year, sep="/")
+  } else {
+    season = paste(year, year + 1, sep="/")
+  
+  }
+  return(season)
+}
+
+
+create_plots <- function(conf){
+
+  current_season <- get_season(Sys.Date())
+  
+  mem_schema$db_connect(sykdomspuls::CONFIG$DB_CONFIG)
+  data <- mem_schema$get_data_db(season==current_season)
+
+
+  folder <- fhi::DashboardFolder("results", sprintf("%s/%s", LatestRawID(),
+                                                    paste("mem",conf$tag, sep="_")))
+  if (!file.exists(folder)){
+    dir.create(folder)
+  }
+
+  
+  for(loc in unique(data[, location])){
+    data_location = data[location==loc]
+    data_location$week <- factor(data_location$week, levels = data_location$week)
+    q <- ggplot(data_location) +
+      geom_line(aes(x=week, y=rate, group=1)) +
+      fhiplot::theme_fhi_lines() +
+      geom_ribbon(aes(x=week, ymin=very_high, ymax=very_high*1.1, fill="l5", group=1), alpha=0.5) +
+      geom_ribbon(aes(x=week, ymin=high, ymax=very_high, fill="l4", group=1), alpha=0.5) +
+      geom_ribbon(aes(x=week, ymin=medium, ymax=high, fill="l3", group=1), alpha=0.5) +
+      geom_ribbon(aes(x=week, ymin=low, ymax=medium, fill="l2", group=1), alpha=0.5) +
+      geom_ribbon(aes(x=week, ymin=0, ymax=low, fill="l1", group=1), alpha=0.5)+
+      fhiplot::scale_fill_fhi("Level", labels=c("l1"="Very low", "l2"="Low", "l3"="Medium",
+                              "l4"="High", "l5"="Very high"),
+                              palette="map_seq_complete", direction=-1) +
+      ggtitle(paste("Percent of patients with ILI for Influenza season",
+                    current_season,
+                    "in",
+                    get_location_name(loc))) + 
+      ylab("% of patients with ILI")
+    
+    
+    filename = paste(folder, "/", loc, ".png", sep="")
+    ggsave(filename, q)
+    
+  }
+
+}
+
+
+
 
 prepare_data_frame <- function(data){
   
@@ -153,6 +225,7 @@ run_mem_model <- function(data){
 mem_results_field_types <- c(
   "tag"="TEXT",
   "location"="TEXT",
+  "season"="TEXT",
 #  "status"="TEXT",
 #  "wkyr"="TEXT",
   "year"="INTEGER",
